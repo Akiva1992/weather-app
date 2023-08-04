83f1d6df9b0866f65d80028b0da3eb5874f7df75
Akiva <breuerakiva@gmail.com>
2023-08-04 20:57:07 +0300

    Changed the get request protocol to https.

--- src/getForecastData.js
+++ src/getForecastData.js
@@ -1,6 +1,6 @@
 export default async function getForecastWeatherData(location) {
     try {
-      const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e0f276e0654440c6b4a212911232107&q=${location}&days=3&aqi=yes&alerts=yes`, { mode: "cors" });
+      const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e0f276e0654440c6b4a212911232107&q=${location}&days=3&aqi=yes&alerts=yes`, { mode: "cors" });
   
       if (!response.ok) {
         throw new Error(response.statusText);
